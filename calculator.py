d18cc8f68607db992f0a4b9ce80212a6ea0a8852
fjay72 <faizaljaffri93@gmail.com>
2021-11-23 13:10:05 -0600

    Adding function to change title head

--- calculator.py
+++ calculator.py
@@ -1,1 +1,26 @@
 from tkinter import *
+# Create a calculator class
+class Calculator:
+
+    def __init__(self, master):
+        '''
+        Dic: Define what to do on initialization
+        '''
+
+        # Assign reference to the main window of the application
+        self.master = master
+
+        # Add a name to our application
+        master.title("Scientific calculator")
+        master.title("Group Project")
+
+        # Create a line where we display the equation
+        self.equation = Entry(master, width=36, borderwidth=5)
+
+        # Assign a position for the equation line in the grey application window
+        self.equation.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+
+        # Execute the .creteButton() method
+        self.createButton()
+
+
